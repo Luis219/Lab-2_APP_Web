edddda89a050e3b7e061a831c5321316c4f15021
pluis9 <luispalcivar@gmail.com>
2022-05-24 23:19:36 -0500

    Parte 4

--- todoapp.py
+++ todoapp.py
@@ -16,39 +16,45 @@ app=Flask(__name__, template_folder='templates')
 def inicio():
     return render_template('inicio.html')
 
+
+tareas=[]
+correos=[]
+prioridades=[]
+
 #Ruta raíz- página enviar con metodo get
+
 @app.route('/enviar',methods=['POST'])
 
 #función que retorna la página enviar
-def enviar():
-
+def enviar(): 
     tarea= request.form.get('tarea')
     correo= request.form.get('correo')
     prioridad= request.form.get('prioridad')
-    tareas=[]
-    tareas.append(tarea)
+
+    if len(tarea)>0 and len(correo)>0:
+        validar_correo()
+        tareas.append(tarea)
+        correos.append(correo)
+        prioridades.append(prioridad)
+    else:
+        return render_template('inicio.html')
  
-    correos=[]
-    correos.append(correo)
-    
-    prioridades=[]
-    prioridades.append(prioridad)
-   
-
-
-
     def validar_correo(correo):
-        expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
-        return re.match(expresion_regular, correo) is not None
-    return render_template('inicio.html', tareas, correos, prioridades)
+        validar = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+        return re.match(validar, correo) is not None
+    return render_template('inicio.html', tareas=tareas, correos=correos, prioridades= prioridades)
 
 #Ruta raíz- página borrar
-@app.route('/borrar')
+@app.route('/borrar',methods=['GET','POST'])
 
 #función que retorna la página borrar
 def borrar():
+    
+    tareas.clear()
+    correos.clear()
+    prioridades.clear()
 
-    return render_template('borrar.html')
+    return render_template('inicio.html',tareas=tareas, correos=correos, prioridades= prioridades)
 
 
 #función principal
