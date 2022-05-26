e5243773b5ddd950ba47ed208cdd82f7abb961ce
pluis9 <luispalcivar@gmail.com>
2022-05-25 23:24:44 -0500

    Se agrego función extra guardar

--- todoapp.py
+++ todoapp.py
@@ -1,18 +1,24 @@
 #se importa escape de markupsafe
+
 import re
+import json
+import os
 from asyncio import mixins
 from markupsafe import escape
 
 #Se importa la librería Flask
-from flask import Flask, abort, render_template, request
+from flask import Flask, abort, redirect, render_template, request, flash, url_for
 
 #variable de instancia app
 app=Flask(__name__, template_folder='templates')
 
+app.secret_key='12345'
+
+
 #Ruta raíz- página inicio
 @app.route('/')
 
-#función que retorna la página
+#función que retorna la página principal
 def inicio():
     return render_template('inicio.html')
 
@@ -21,33 +27,34 @@ tareas=[]
 correos=[]
 prioridades=[]
 
-#Ruta raíz- página enviar con metodo get
+#Ruta hacia la función enviar
 
 @app.route('/enviar',methods=['POST'])
 
-#función que retorna la página enviar
+#función que permite enviar los datos ingresados hacia la tabla
 def enviar(): 
     tarea= request.form.get('tarea')
     correo= request.form.get('correo')
     prioridad= request.form.get('prioridad')
+  
 
+    #validar que se ingresen datos
     if len(tarea)>0 and len(correo)>0:
-        validar_correo()
+       
+       #se ingresen los datos a las listas
         tareas.append(tarea)
         correos.append(correo)
         prioridades.append(prioridad)
     else:
+        flash('Error al ingresar los datos')
         return render_template('inicio.html')
  
-    def validar_correo(correo):
-        validar = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
-        return re.match(validar, correo) is not None
     return render_template('inicio.html', tareas=tareas, correos=correos, prioridades= prioridades)
 
-#Ruta raíz- página borrar
-@app.route('/borrar',methods=['GET','POST'])
+#Ruta hacia función borrar
+@app.route('/borrar',methods=['POST'])
 
-#función que retorna la página borrar
+#función que permite borrar las tareas de toda la tabla
 def borrar():
     
     tareas.clear()
@@ -55,6 +62,55 @@ def borrar():
     prioridades.clear()
 
     return render_template('inicio.html',tareas=tareas, correos=correos, prioridades= prioridades)
+
+@app.route('/guardar',methods=['POST'])
+
+#función que permite borrar las tareas de toda la tabla
+def guardar():
+
+    tarea= request.form.get('tarea')
+    correo= request.form.get('correo')
+    prioridad= request.form.get('prioridad')
+
+    tareas.append(tarea)
+    correos.append(correo)
+    prioridades.append(prioridad)
+
+   
+  
+    path, _=os.path.split(os.path.abspath(__file__))
+    
+    data={}
+    data['tareas']=[]
+    data['correos']=[]
+    data['prioridad']=[]    
+    
+    data["tareas"].append(tareas)
+    data["correos"].append(correos)
+    data["prioridad"].append(prioridades)
+
+   
+    with open(path+f'/datos.json','w') as file:
+        json.dump(data, file, indent=4)
+
+    return render_template('guardar.html')
+
+
+@app.route('/ver_datos',methods=['POST'])
+
+#función que permite borrar las tareas de toda la tabla
+def ver_datos():
+    
+    path, _=os.path.split(os.path.abspath(__file__))
+    
+   
+    with open(path+'/datos.json') as file:
+       data= json.load(file)
+    
+    flash(data)
+
+    return render_template('guardar.html')
+
 
 
 #función principal
